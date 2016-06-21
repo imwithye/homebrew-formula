769db9e3bcd2aeee84ec2ad59c116378cf5213b3
Ciel <imwithye@gmail.com>
2016-06-21 14:30:04 +0200

    Update git-ignore.rb

--- Formula/git-ignore.rb
+++ Formula/git-ignore.rb
@@ -1,17 +1,11 @@
-require 'formula'
-
 class GitIgnore < Formula
-  homepage 'https://github.com/imwithye/git-ignore'
+	desc "A simple tool to manage your git ignore files."
+	homepage "https://github.com/imwithye/gitignore"
+	version "v1.0.2"
+	url "https://github.com/imwithye/gitignore/releases/download/#{version}/git-ignore_darwin_amd64"
+	sha256 "1d53a84720a129c16e91ebe3a16b8dc687d87b72ffad7cfc2c5f1871ecf0bd54"
 
-  # Use the tag instead of the tarball to get submodules
-  url 'https://github.com/imwithye/git-ignore.git', :tag => 'v0.3'
-
-  def install
-    prefix.install "git-ignore" => "core"
-    bin.install_symlink "#{prefix}/core/git_ignore.py" => "git-ignore"
-  end
-  
-  test do
-    system "#{bin}/git-ignore"
-  end
+	def install
+		bin.install "git-ignore_darwin_amd64" => "git-ignore"
+	end
 end

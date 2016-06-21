2947a428f89536a62ed6a53008c09bf42418df8a
Ciel <imwithye@gmail.com>
2016-06-21 15:05:50 +0200

    Update git-ignore.rb

--- Formula/git-ignore.rb
+++ Formula/git-ignore.rb
@@ -1,7 +1,7 @@
 class GitIgnore < Formula
 	desc "A simple tool to manage your git ignore files."
 	homepage "https://github.com/imwithye/gitignore"
-	version "v1.0.2"
+	version "v1.0.3"
 	url "https://github.com/imwithye/gitignore/releases/download/#{version}/git-ignore_darwin_amd64"
 	sha256 "1d53a84720a129c16e91ebe3a16b8dc687d87b72ffad7cfc2c5f1871ecf0bd54"
 
